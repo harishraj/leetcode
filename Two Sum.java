485f405b403e03e2e0013f449699d99e8748fd38
Yadong Wen <ralph.wen@gmail.com>
2014-08-21 22:45:02 +0800

    v2 time O(n) space O(n) Accepted
    two optimizations:
    
    1) from HashMap.containsValue to HashMap.containsKey
    2) from two passes to one pass
    
    v1 is actually O(n^2) time complexity because HashMap.containsValue if O(n) instead of O(1)

--- Two Sum.java
+++ Two Sum.java
@@ -1,21 +1,17 @@
-// Two Sum
-
 import java.util.HashMap;
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(numbers.length);
         for(int i = 0; i < numbers.length; i++) {
-            map.put(i, numbers[i]);
-        }
-        for(int i = 0; i < numbers.length; i++) {
-            if(map.containsValue(target-numbers[i])) {
-                for(int j=i+1; j<numbers.length; j++) {
-                    if(numbers[i]+numbers[j]==target) {
-                        return new int[]{i+1, j+1};
+            if(map.containsKey(target-numbers[i])) {
+                for(int j=0; j<i; j++) {
+                    if(numbers[i]+numbers[j]==target){
+                        return new int[]{j+1, i+1};
                     }
                 }
             }
+            map.put(numbers[i], i);
         }
         return new int[]{-1,-1};
     }
