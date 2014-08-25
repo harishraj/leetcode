731126ac7338bcac83c07d72fe03a7783901c911
Yadong Wen <ralph.wen@gmail.com>
2014-08-25 09:39:45 +0800

    v2 time O(n) space O(1) Accepted
    case 6 when A.length == 0 was not considered in v1

--- Remove Duplicates from Sorted Array.java
+++ Remove Duplicates from Sorted Array.java
@@ -1,7 +1,7 @@
 public class Solution {
     public int removeDuplicates(int[] A) {
         if(A == null) return 0;
-        if(A.length == 1) return 1;
+        if(A.length <= 1) return A.length;
         int i = 0, j = 1;
         while(j < A.length) {
             if(A[j] != A[i]) {
@@ -13,6 +13,7 @@ public class Solution {
             j++;
         }
         return i + 1;
+        
         /**
          * test
          * 
@@ -21,6 +22,7 @@ public class Solution {
          * 3. [1 1 2]
          * 4. [1 2 1] invalid input
          * 5. [1 2 2]
+         * 6. []
          */
     }
 }
