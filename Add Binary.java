fa9cd4ae8f031ffdfda4a806442e66b870d315cf
Yadong Wen <ralph.wen@gmail.com>
2014-09-01 12:58:01 +0800

    v2 time O(n) space O(n) Accepted

--- Add Binary.java
+++ Add Binary.java
@@ -8,21 +8,21 @@ public class Solution {
         int carry = 0;
         StringBuilder sb = new StringBuilder();
         while(i>=0&&j>=0) {
-            int ai=Integer.valueOf(a.charAt(i));
-            int bj=Integer.valueOf(b.charAt(j));
+            int ai=Integer.valueOf(a.charAt(i)-'0');
+            int bj=Integer.valueOf(b.charAt(j)-'0');
             sb.append((ai+bj+carry)%2);
             carry=(ai+bj+carry)/2;
             i--;
             j--;
         }
         while(i>=0){
-            int ai=Integer.valueOf(a.charAt(i));
+            int ai=Integer.valueOf(a.charAt(i)-'0');
             sb.append((ai+carry)%2);
             carry=(ai+carry)/2;
             i--;
         }
         while(j>=0){
-            int bj=Integer.valueOf(b.charAt(j));
+            int bj=Integer.valueOf(b.charAt(j)-'0');
             sb.append((bj+carry)%2);
             carry=(bj+carry)/2;
             j--;
