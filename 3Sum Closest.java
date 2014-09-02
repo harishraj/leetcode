54d035ff9d981b29eaef1d7b194d5a11d0f827ed
Yadong Wen <ralph.wen@gmail.com>
2014-09-02 18:46:31 +0800

    v1 runtime error!

---
+++ 3Sum Closest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int threeSumClosest(int[] num, int target) {
+        // time O(n^2log(n)) space O(log(n))
+        Arrays.sort(num);
+        int diff = 0x7fffffff;
+        for(int i = 0; i < num.length; i++) {
+            for(int j = i+1; j < num.length; j++) {
+                int t = target - num[i] - num[j];
+                int k = Arrays.binarySearch(num, j+1, num.length, t);
+                if(k>=0) return target;
+                else {
+                    if(Math.abs(target-num[~k]) < Math.abs(diff)) diff = target-num[~k];
+                    if((~k)!=0 && Math.abs(target-num[(~k)-1]) < Math.abs(diff)) diff = target - num[(~k)-1];
+                }
+            }
+        }
+        return target - diff;
+    }
+}
