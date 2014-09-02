cd374f4dc5835b1b223ee846422b6906cc57b0bd
Yadong Wen <ralph.wen@gmail.com>
2014-09-03 01:17:51 +0800

    v3 time O(log(n)) space O(log(n)) Accepted

--- Pow(x, n).java
+++ Pow(x, n).java
@@ -1,5 +1,6 @@
 public class Solution {
     public double pow(double x, int n) {
+        if(n==0x80000000) return pow(x, n/2)*pow(x, n/2);
         if(n==0) return 1;
         if(n==1) return x;
         if(n<0) return 1.0/pow(x, -n);
