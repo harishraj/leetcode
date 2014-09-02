80df0bdd4cd089370e9231d05766f6b400f3ed87
Yadong Wen <ralph.wen@gmail.com>
2014-09-02 14:05:13 +0800

    v2 time O(n) space O(1) Accepted

--- Remove Duplicates from Sorted Array II.java
+++ Remove Duplicates from Sorted Array II.java
@@ -4,17 +4,19 @@ public class Solution {
         
         if(A.length==0) return 0;
         int r = 1;
-        
+        int c = 1;
         boolean twice=false;
         for(int i=1; i<A.length; i++) {
             if(A[i]==A[i-1]) {
                 if(!twice) {
                     r++;
                     twice=true;
+                    A[c++]=A[i];
                 }
             } else {
                 r++;
                 twice=false;
+                A[c++]=A[i];
             }
         }
         return r;
