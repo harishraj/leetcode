8c3a02cac060370a4060c9628d89b68ca46bafd3
Yadong Wen <ralph.wen@gmail.com>
2014-09-03 09:50:12 +0800

    v2 wrong answer!

--- Path Sum.java
+++ Path Sum.java
@@ -9,8 +9,7 @@
  */
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-        if(root==null && sum!=0) return false;
-        if(root.left==null && root.right==null) return sum==root.val;
+        if(root==null) return sum==0;
         return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
     }
 }
