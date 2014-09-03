f767fcf77cccaa2ae7542d4e98bf0e570de09083
Yadong Wen <ralph.wen@gmail.com>
2014-09-04 00:51:47 +0800

    v1 wrong answer!

---
+++ Implement strStr().java
@@ -0,0 +1,20 @@
+public class Solution {
+    public String strStr(String haystack, String needle) {
+        String h = haystack, n = needle;
+        if(h==null || n==null) return null;
+        if(h.length()<n.length()) return null;
+        if(n.equals("")) return h;
+        for(int i=0; i<=h.length()-n.length(); i++) {
+             boolean match = true;
+             for(int j=0; j<n.length(); j++) {
+                 if(h.charAt(i+j)!=n.charAt(j)){
+                     i+=j;
+                     match=false;
+                     break;
+                 }
+             }
+             if(match) return h.substring(i);
+        }
+        return null;
+    }
+}
