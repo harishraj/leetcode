6bd1ff268d33d0ff03008330297a95a8ffe4cafc
Yadong Wen <ralph.wen@gmail.com>
2014-09-07 15:43:31 +0800

    v1 time O(2^n) space O(2^n) Accepted

--- Gray Code.java
+++ Gray Code.java
@@ -8,12 +8,16 @@ public class Solution {
         0000 0001 0011 0010 0110 0111 0101 0100 1100 1101 1111 1110 1010 1011 1001 1000
         */
         List<Integer> r = new ArrayList<Integer>();
-        if(n <= 0) return r;
+        if(n < 0) return r;
+        if(n == 0) {
+            r.add(0);
+            return r;
+        }
         r.add(0); r.add(1);
         for(int i=1; i<n; i++) {
             int len = r.size();
             for(int j=len-1; j>=0; j--) {
-                r.add(1<<i + r.get(j));
+                r.add((1<<i) + r.get(j));
             }
         }
         return r;
