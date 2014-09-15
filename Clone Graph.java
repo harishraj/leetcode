59f06ac87adcc05cc7d7ad4a5eb398b7cbeaea89
Yadong Wen <ralph.wen@gmail.com>
2014-09-15 20:15:51 +0800

    v5 time O(V+E) space O(V) Accepted

--- Clone Graph.java
+++ Clone Graph.java
@@ -11,83 +11,50 @@ import java.util.*;
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         /*
-            queue q
-            
-            hashset visited
-            q.offer node
-            while q not empty:
-                n=q.poll
-                n.neighbors.add new UndirectedGraphNode(n.label)
-                visited.add n
-                for nn in n.neighbors not including the last one:
-                    if not visited.contains nn:
-                        q.offer nn
-            
-            visited.clear
-            q.offer node
-            while q not empty:
-                n=q.poll
-                visited.add n
-                for nn in n.neighbors not including the last one:
-                    if not visited.contains nn:
-                        q.offer nn
-                    n.neighbors.last.neighbors.add(nn.neighbors.last)
-            
-            r = node.neighbors.last
-            
-            visited.clear
-            q.offer node
-            while q not empty:
-                n=q.poll
-                n.neigbors.removeLast
-                visited.add n
-                for nn in n.neighbors:
-                    if not visited.contains nn:
-                        q.offer nn
-            
-            return r
+            def cloneGraph(self, node):
+                if node is None:
+                    return None
+                q=[]
+                copied={}
+                q.append(node)
+                copied[node] = UndirectedGraphNode(node.label)
+                while q:
+                    n = q.pop()
+                    for nb in n.neighbors:
+                        if copied.__contains__(nb):
+                            copied[n].neighbors.append(copied[nb])
+                        else:
+                            new_nb = UndirectedGraphNode(nb.label)
+                            copied[n].neighbors.append(new_nb)
+                            copied[nb]=new_nb
+                            q.append(nb)
+                return copied[node]
+                
+                time O(V+E) space O(V)
         */
         if(node==null) return null;
         ArrayDeque<UndirectedGraphNode> q = new ArrayDeque<UndirectedGraphNode>();
-        HashSet<UndirectedGraphNode> visited = new HashSet<UndirectedGraphNode>();
-        q.offer(node);
+        HashMap<UndirectedGraphNode,UndirectedGraphNode> map = 
+            new HashMap<UndirectedGraphNode,UndirectedGraphNode>();
+        q.push(node);
+        map.put(node, new UndirectedGraphNode(node.label));
         while(!q.isEmpty()) {
-            UndirectedGraphNode n = q.poll();
-            UndirectedGraphNode newn = new UndirectedGraphNode(n.label);
-            n.neighbors.add(newn);
-            visited.add(n);
-            for(UndirectedGraphNode nn : n.neighbors) {
-                if(nn != newn && !visited.contains(nn)) q.offer(nn);
-            }
-        }
-        
-        visited.clear();
-        q.offer(node);
-        while(!q.isEmpty()) {
-            UndirectedGraphNode n = q.poll();
-            visited.add(n);
-            UndirectedGraphNode newn = n.neighbors.get(n.neighbors.size()-1);
-            for(UndirectedGraphNode nn : n.neighbors) {
-                if(nn != newn) {
-                    if(!visited.contains(nn)) q.offer(nn);
-                    newn.neighbors.add(nn.neighbors.get(nn.neighbors.size()-1));
+            UndirectedGraphNode n = q.pop();
+            for(UndirectedGraphNode nb : n.neighbors) {
+                if(map.containsKey(nb)) {
+                    UndirectedGraphNode nn = map.get(n);
+                    nn.neighbors.add(map.get(nb));
+                    map.put(n,nn);
+                } else {
+                    UndirectedGraphNode newNb = new UndirectedGraphNode(nb.label);
+                    UndirectedGraphNode nn = map.get(n);
+                    nn.neighbors.add(newNb);
+                    map.put(n,nn);
+                    map.put(nb,newNb);
+                    q.push(nb);
                 }
             }
         }
-        
-        UndirectedGraphNode r = node.neighbors.get(node.neighbors.size()-1);
-        
-        visited.clear();
-        q.offer(node);
-        while(!q.isEmpty()) {
-            UndirectedGraphNode n = q.poll();
-            n.neighbors.remove(n.neighbors.size()-1);
-            visited.add(n);
-            for(UndirectedGraphNode nn : n.neighbors) {
-                if(!visited.contains(nn)) q.offer(nn);
-            }
-        }
-        
-        return r;
+        return map.get(node);
     }
 }
